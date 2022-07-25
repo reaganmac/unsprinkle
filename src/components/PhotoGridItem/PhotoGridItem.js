7034bd7b978617ce89cf848bf05cf00a465e2eb6
reaganmac <reagan@rmac.dev>
2022-07-24 21:38:27 -0500

    Alter tag styles to fit text on single line

--- src/components/PhotoGridItem/PhotoGridItem.js
+++ src/components/PhotoGridItem/PhotoGridItem.js
@@ -48,12 +48,19 @@ const Image = styled.img`
 `;
 
 const Tags = styled.ul`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 8px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  li {
+    margin-right: 8px;
+  }
+  li:last-of-type {
+    margin-right: 0px;
+  }
 `;
 
 const Tag = styled.li`
+  display: inline;
   padding: 4px 8px;
   background: var(--color-gray-300);
   font-size: 0.875rem;
